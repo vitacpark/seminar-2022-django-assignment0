f1135baf34caf5c7af2f4651f490bde8a4460584
vitacpark <vitacpark@snu.ac.kr>
2022-09-19 03:09:47 +0900

    Assignment02 Completed!

---
+++ problem-2/djangoProject/polls/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from . import views
+from django.http import HttpResponse
+
+from .models import Question
+
+
+
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
